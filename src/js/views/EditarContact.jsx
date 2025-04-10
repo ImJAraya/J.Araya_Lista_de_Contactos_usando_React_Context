88890e45d0407e95e881cf8a1a0ef600d901a896
ImJAraya <fabiarayamontero@gmail.com>
2025-04-10 09:55:45 -0600

    terminado

--- src/js/views/EditarContact.jsx
+++ src/js/views/EditarContact.jsx
@@ -10,10 +10,10 @@ const EditarContact = () => {
     const { index } = useParams()
     const i = parseInt(index)
     let contactoActual = store.demo[i]
-    const [nombre, setNombre] = useState("")
-    const [email, setEmail] = useState("")
-    const [address, setAddress] = useState("")
-    const [telefono, setTelefono] = useState("")
+    const [nombre, setNombre] = useState(contactoActual.name)
+    const [email, setEmail] = useState(contactoActual.email)
+    const [address, setAddress] = useState(contactoActual.address)
+    const [telefono, setTelefono] = useState(contactoActual.phone)
 
     const handlerClick = () => {
         let contactoEditado = {}
@@ -40,29 +40,23 @@ const EditarContact = () => {
                     <div  >
                         <img className="imagenRedonda" src="https://picsum.photos/200" />
                     </div>
-                    <div>
-                        <h1>{contactoActual.name}</h1>
-                        <p>{contactoActual.phone}</p>
-                        <p>{contactoActual.address}</p>
-                        <p>{contactoActual.email}</p>
-                    </div>
                 </div>
                 <form className="row g-3">
                     <div className="col-md-6">
                         <label htmlFor="nombre" className="form-label">Nombre</label>
-                        <input type="text" className="form-control" id="nombre" onChange={(e) => setNombre(e.target.value)} />
+                        <input type="text" className="form-control" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
                     </div>
                     <div className="col-md-6">
                         <label htmlFor="inputEmail" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="inputEmail" onChange={(e) => setEmail(e.target.value)} />
+                        <input type="email" className="form-control" id="inputEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className="col-12">
                         <label htmlFor="inputAddress" className="form-label">Address</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" onChange={(e) => setAddress(e.target.value)} />
+                        <input type="text" className="form-control" id="inputAddress" value={address} placeholder="1234 Main St" onChange={(e) => setAddress(e.target.value)} />
                     </div>
                     <div className="col-12">
                         <label htmlFor="numeroTelefono" className="form-label">Telefono</label>
-                        <input type="number" className="form-control" id="numeroTelefono" placeholder="Numero de telefono" onChange={(e) => setTelefono(e.target.value)} />
+                        <input type="number" className="form-control" id="numeroTelefono" value={telefono} placeholder="Numero de telefono" onChange={(e) => setTelefono(e.target.value)} />
                     </div>
                     <div className="col-12">
                         <button
